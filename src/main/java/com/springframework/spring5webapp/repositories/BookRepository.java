e6351e7879f52bb988a3e3a9e8aea54539d54066
RahulD <rahul.deore@hotmail.com>
2019-07-07 17:19:20 -0400

    Added Spring data repositories interface. Spring will wire them during startup and they are ready for me to do basic CRUD operation without writing any code

---
+++ src/main/java/com/springframework/spring5webapp/repositories/BookRepository.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2019. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+ * Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.
+ * Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.
+ * Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus.
+ * Vestibulum commodo. Ut rhoncus gravida arcu.
+ */
+
+package com.springframework.spring5webapp.repositories;
+
+import com.springframework.spring5webapp.model.Book;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Created by Rahul on 2019-07-07
+ */
+public interface BookRepository extends CrudRepository<Book, Long> {
+}
